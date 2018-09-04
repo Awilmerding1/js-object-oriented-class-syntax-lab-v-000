fa6c9ff53ee9ac9bd6be693376b1f2a243192fd4
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-04 15:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class BoardMember {
     return 'You will see great things from Scuber.'
   }
   
-  sayHi () {
+  sayHi() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${training}.`
   }
   
